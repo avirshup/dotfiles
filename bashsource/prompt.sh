8c55419f2880085313af95693e3c13dc4deeb97f
Aaron Virshup <aaronvirshup@gibarian.attlocal.net>
2017-05-10 20:00:21 -0700

    Show pyenv + path

--- bashsource/prompt.sh
+++ bashsource/prompt.sh
@@ -1,4 +1,47 @@
+function get_dir(){
+	thisdir='75'
+	if [[ ${PWD} == ${HOME} ]]; then
+		echo $(colornum '~' ${thisdir})
+	else
+		subdir=$(dirname ${PWD})
+	    if [[ ${subdir} == ${HOME} ]]
+	    then subdir="~"
+	    else subdir=$(basename ${subdir})
+	    fi
+		echo "$(colornum ${subdir} ${defcolor})${slash}$(colornum $(basename ${PWD}) ${thisdir})";
+    fi
+}
 
-unset PROMPT_COMMAND
-PROMPT_ANSI='33m'
-export PS1='\[\e]2;\h:${PWD}\a\e[${PROMPT_ANSI}\][\u@\h \W]\$\[\e[m\] '
+function get_pyenv(){
+	[ ${PYENV_VERSION} ] && echo ${PYENV_VERSION} || cat ${HOME}/.pyenv/version
+}
+
+function color_pyenv(){
+	pv=$(get_pyenv)
+	if [ ${pv:0:1} == '3' ]
+	then num="99"
+	else num="34"
+	fi
+	echo $(colornum $pv $num)
+}
+
+show_tput_colors(){
+	for C in {0..255}; do
+    tput setab $C
+    echo -n "$C "
+	done
+	tput sgr0
+	echo
+}
+
+function colornum(){
+	echo "\[$(tput setaf $2)\]$1\[$(tput sgr0)\]"
+}
+
+defcolor="246"
+start=$(colornum "[" $defcolor)
+end=$(colornum "]\\$" $defcolor)
+slash=$(colornum '/' $defcolor)
+
+
+PROMPT_COMMAND='export PS1="${start}$(color_pyenv) $(get_dir)${end} "'
