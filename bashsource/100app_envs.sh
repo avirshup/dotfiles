c56ba52c59576883a91886e19e3657f044651a64
Aaron Virshup <avirshup@gmail.com>
2017-10-26 21:06:27 -0700

    Less fail without pyenv

--- bashsource/100app_envs.sh
+++ bashsource/100app_envs.sh
@@ -5,8 +5,14 @@ eval "$(nodenv init -)"
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+
+if which pyenv &>/dev/null; then
+   eval "$(pyenv init -)"
+   eval "$(pyenv virtualenv-init -)"
+else
+  echo "pyenv not installed"
+fi
+
 
 # bashmarks
 source ~/.local/bin/bashmarks.sh
