db0448a424f6fbcc626dcc65c406dd6bd2a9961c
Aaron Virshup <avirshup@gmail.com>
2017-10-26 22:08:58 -0700

    add bistream vera sans install

--- installers/999other.sh
+++ installers/999other.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
-
 Open "http://www.ks.uiuc.edu/Development/Download/download.cgi?UserID=&AccessCode=&ArchiveID=1476"
 
+echo "Installing BitStream Vera Sans"
+cd /tmp
+wget http://ftp.gnome.org/pub/GNOME/sources/ttf-bitstream-vera/1.10/ttf-bitstream-vera-1.10.tar.gz
+tar xvzf ttf-bitstream-vera-1.10.tar.gz
+mv ttf-bitstream-vera-1.10/*.ttf $HOME/Library/Fonts
