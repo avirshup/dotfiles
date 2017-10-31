9d73d8f47ad8b5c4ad725e169d14adb51c5cbe52
Aaron Virshup <avirshup@gmail.com>
2017-10-31 14:28:49 -0700

    Add vi settings

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -6,39 +6,43 @@ else
   echocmd ln -s ${PWD}/etc/mackup.cfg $HOME/.mackup.cfg
 fi
 
-flagstart="# AMV dotfile setup -- do not edit"
-flagend="# end AMV dotfile setup -- do not edit"
-
-bash_profile_loader="export DOTFILE_HOME=${PWD}
-source \${DOTFILE_HOME}/sourceme.sh"
-
-git_loader="[include]
-   path = ${PWD}/etc/gitconfig
-"
+flagstart="AMV dotfile setup -- do not edit"
+flagend="end AMV dotfile setup -- do not edit"
 
 function add-to-file(){
 	target=$1
 	content=$2
+
+	if [ -z $3 ]; then
+	    comment="\#"
+	else
+	    comment=$3
+	fi
+
 	if grep "$flagstart" $target &> /dev/null
 		then
 		echo "Loading hooks already detected in ${target}"
 	else
 		echo >> $target
-		echo "$flagstart -- ${DOTFILE_HOME}" >> ${target}
+		echo "$comment $flagstart -- ${DOTFILE_HOME}" >> ${target}
 		echo "Adding loading hooks to ${target}"
 		echo "$content" >> $target
-		echo "$flagend" >> $target
+		echo "$comment $flagend" >> $target
 	fi
 }
 
+# INPUTRC
 add-to-file "${HOME}/.inputrc" "\$include ${PWD}/etc/inputrc"
-add-to-file "${HOME}/.bash_profile" "$bash_profile_loader"
-add-to-file "${HOME}/.gitconfig" "$git_loader"
 
+# BASH_PROFILE
+add-to-file "${HOME}/.bash_profile" "export DOTFILE_HOME=${PWD}
+source \${DOTFILE_HOME}/sourceme.sh"
 
+# GITCONFIG
+add-to-file "${HOME}/.gitconfig" "[include]
+   path = ${PWD}/etc/gitconfig"
 
-
-
-
+# VIMRC
+add-to-file "${HOME}/.vimrc" ":source ${PWD}/etc/vimrc" '"'
 
 
