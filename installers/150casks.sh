4f9946755b87481a760ea8369128ca5f11e42d82
Aaron Virshup <avirshup@gmail.com>
2017-11-10 21:12:06 -0800

    Add slate

--- installers/150casks.sh
+++ installers/150casks.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-for app in "google-chrome
-       	sublime-text
-	jet
-	docker
-	sourcetree 
-	istat-menus
-	avogadro
-	pycharm-ce
-	bettertouchtool
-	mactex
-	latexit
-	texmaker
-	sketch
-	pinegrow-web-designer"; do
+# pinegrow-web-designer -- doesn't appear to work currently \
+
+
+for app in \
+    google-chrome \
+    sublime-text \
+	jet \
+	docker \
+	sourcetree \
+	istat-menus \
+	avogadro \
+	pycharm-ce \
+	bettertouchtool \
+	mactex \
+	latexit \
+	texmaker \
+	sketch \
+	slack \
+	webstorm \
+    slate \
+	steermouse; do
 	
 	echocmd brew cask install $app
 done
