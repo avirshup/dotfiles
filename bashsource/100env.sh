aea59ed85734c95189256336be799bcbf5a78d6e
Aaron Virshup <avirshup@gmail.com>
2021-12-03 14:41:34 -0800

    stuff

--- bashsource/100env.sh
+++ bashsource/100env.sh
@@ -3,7 +3,7 @@ prepend-pathvar PATH $HOME/bin
 prepend-pathvar PATH $HOME/.local/bin
 
 # completion
-if command -v brew; then
+if command -v brew &> /dev/null; then
     if [ -f $(brew --prefix)/etc/bash_completion ]; then
         . $(brew --prefix)/etc/bash_completion
     fi
@@ -15,22 +15,24 @@ export HISTCONTROL="$HISTCONTROL:ignoredups"
 export HISTIGNORE="ls:ls -l:top"
 
 
-# nodenv
-export NODENV_ROOT="$HOME/.nodenv"
-if [ -f ${NODENV_ROOT}/bin/nodenv ]; then
-   prepend-pathvar PATH ${NODENV_ROOT}/bin
-   eval "$(nodenv init -)"
-else
-	echo "nodenv not installed"
-fi
+# # nodenv
+# export NODENV_ROOT="$HOME/.nodenv"
+# if [ -f ${NODENV_ROOT}/bin/nodenv ]; then
+#    prepend-pathvar PATH ${NODENV_ROOT}/bin
+#    eval "$(nodenv init -)"
+# else
+# 	echo "nodenv not installed"
+# fi
 
 
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 if [ -f ${PYENV_ROOT}/bin/pyenv ]; then
    prepend-pathvar PATH ${PYENV_ROOT}/bin
-   eval "$(pyenv init -)"
+   eval "$(pyenv init --path)"
    eval "$(pyenv virtualenv-init -)"
+   eval "$(pyenv init -)"
 else
   echo "pyenv not installed"
 fi
--- bashsource/300aliases.sh
+++ bashsource/300aliases.sh
@@ -1,12 +1,12 @@
-function make-alias(){
-   name=$1
-   target=$2
+function make-alias() {
+	name=$1
+	target=$2
 
-   if which $target &> /dev/null; then
-	alias $name=$target
-   else
-	echo "$target not installed"
-   fi
+	if which $target &>/dev/null; then
+		alias $name=$target
+	else
+		echo "$target not installed"
+	fi
 }
 
 make-alias diff colordiff
@@ -15,7 +15,7 @@ make-alias vi vim
 alias strip-ansi="perl -pe 's/\x1b\[[0-9;]*m//g'"
 alias tf="terraform"
 alias pyshell="pyenv shell"
-alias py.test="py.test -rEf" 
+alias py.test="py.test -rEf"
 
 alias myps="ps x | grep -v /usr/libexec | grep -v '\.app' | grep -v '/Library'"
 
@@ -25,25 +25,34 @@ if [ "$(uname)" == "Darwin" ]; then
 	}
 
 	ql() {
-		qlmanage -p "$@" &> /dev/null
+		qlmanage -p "$@" &>/dev/null
 	}
 fi
 
+function _click_autocomplete() {
+	if [ ! -z "$1" ] && (command -v "$1" >/dev/null); then
+		export _DEV_QUICKLOAD=1
+		echo "Setting up autocomplete for '$1' command ..."
+		acscript="$(eval "_${1^^}_COMPLETE=bash_source $1")"
+		eval "$acscript"
+	else
+		echo "Please provide click command to autocomplete"
+	fi
+}
 
-function pyscf-activate(){
+function pyscf-activate() {
 	if [ "${_pyscfactive}" != "1" ]; then
 		PYSCFBASE=/Users/aaronvirshup/python_pkg/pyscf_pkg
 
-	    prepend-pathvar DYLD_LIBRARY_PATH ${PYSCFBASE}/pyscf/lib/deps/lib
-	    prepend-pathvar PYTHONPATH ${PYSCFBASE}
+		prepend-pathvar DYLD_LIBRARY_PATH ${PYSCFBASE}/pyscf/lib/deps/lib
+		prepend-pathvar PYTHONPATH ${PYSCFBASE}
 		export _pyscfactive=1
 	fi
 }
 
 alias sctl=supervisorctl
 
-
-function tempdir(){
+function tempdir() {
 	name="$1"
 	if [ -z "$name" ]; then
 		echo "please provide a name."
