f9be7700cc717140b7f777e2708af6860f2ee23b
Aaron Virshup <avirshup@gmail.com>
2022-01-04 12:37:48 -0800

    linux fixes

--- sourceme.sh
+++ sourceme.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # note: $DOTFILE_HOME must already be set
 
-_current_shell=$(sh -c 'ps -p $$ -o ppid=' | xargs ps -o comm= -p)
-export CURRENT_SHELL=${_current_shell#-}
+
+if [[ -n "$ZSH_NAME" ]]; then
+    CURRENT_SHELL=zsh
+elif [[ -n "$BASH" ]]; then
+    CURRENT_SHELL=bash
+else
+    echo "WARNING: not sure what sort of shell this is. Falling back to 'sh'"
+    CURRENT_SHELL=sh
+fi
+export CURRENT_SHELL 
 
 _extensions="sh"
 case "${CURRENT_SHELL}" in 
@@ -15,7 +23,7 @@ case "${CURRENT_SHELL}" in
 esac
 
 export PATH=${DOTFILE_HOME}/bin:${PATH}
-echo "Loading: "
+echo "Loading ${CURRENT_SHELL} environment: "
 # "echo" hack for zsh/bash compatibility
 for _ext in $(echo $_extensions); do
   for _f in ${DOTFILE_HOME}/shell_setup/*.${_ext}; do
